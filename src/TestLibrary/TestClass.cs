27c37334c123487fb40c6eb1710d8d13cddb8b14
Jon Sagara <jonsagara@gmail.com>
2025-04-28 07:13:44 -0700

    Initial commit.

---
+++ src/TestLibrary/TestClass.cs
@@ -0,0 +1,8 @@
+﻿namespace TestLibrary;
+
+public class TestClass
+{
+    // Does not compile: The type or namespace name 'SR' does not exist in the namespace 'TestLibrary' (are you missing an assembly reference?)
+    public static string GetWelcomeMesssage()
+        => TestLibrary.SR.Welcome;
+}
